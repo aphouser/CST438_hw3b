7de40a9a29544418aed7544a23d0804813ca2bfe
Adam Houser <aphouser@gmail.com>
2020-07-11 14:00:24 -0700

    Initial commit

---
+++ src/main/java/cst438hw3b/Cst438Hw3bApplication.java
@@ -0,0 +1,13 @@
+package cst438hw3b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cst438Hw3bApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Cst438Hw3bApplication.class, args);
+  }
+
+}
---
+++ src/test/java/cst438hw3b/Cst438Hw3bApplicationTests.java
@@ -0,0 +1,13 @@
+package cst438hw3b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cst438Hw3bApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
